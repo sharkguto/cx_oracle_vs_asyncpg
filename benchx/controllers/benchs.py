256ed8e606f028cf244962c4e84a27b5898ee76e
Gustavo Freitas <gustavo@gmf-tech.com>
2020-09-29 21:38:32 -0300

    test

--- benchx/controllers/benchs.py
+++ benchx/controllers/benchs.py
@@ -10,6 +10,7 @@ from typing import List
 from fastapi import APIRouter
 from starlette.responses import Response
 from benchx.utils.postconn import p_database
+from benchx.utils.oracleconn import o_database
 
 router = APIRouter()
 
@@ -29,4 +30,6 @@ async def get_friends_from_person(response: Response) -> List:
     Bench oracle cx driver
     """
 
-    return [{"ok": 1}]
+    result = await o_database.fetch_all("select * from system.company")
+    return result
+
